111a32c7eb01ff01dcde5180fbe4bab9ef72c684
dge9788 <dge9788@rit.edu>
2013-01-24 17:24:33 -0500

    Working on Twilio handling. Need to make sure Twilio's HTTP requests are majicked properly by Spring.

--- src/main/java/edu/rit/asksg/dataio/TwilioProvider.java
+++ src/main/java/edu/rit/asksg/dataio/TwilioProvider.java
@@ -3,55 +3,67 @@ package edu.rit.asksg.dataio;
 import edu.rit.asksg.domain.Conversation;
 import edu.rit.asksg.domain.Message;
 import edu.rit.asksg.domain.ProviderConfig;
+import edu.rit.asksg.domain.TwilioSmsRequest;
 import edu.rit.asksg.service.ConversationService;
+import org.hsqldb.lib.StringInputStream;
 import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TwilioProvider implements ContentProvider {
 
-    @Autowired
-    ProviderConfig config;
+	@Autowired
+	ProviderConfig config;
 
-    @Autowired
-    ConversationService conversationService;
+	@Autowired
+	ConversationService conversationService;
 
-    @Override
-    public List<Conversation> fetchNewContent() {
-        return null;
-    }
+	@Override
+	public List<Conversation> fetchNewContent() {
+		return new ArrayList<Conversation>();
+	}
 
-    @Override
-    public List<Conversation> fetchContentSince(LocalDateTime datetime) {
-        return null;
-    }
+	@Override
+	public List<Conversation> fetchContentSince(LocalDateTime datetime) {
+		return new ArrayList<Conversation>();
+	}
 
-    @Override
-    public boolean postContent(Message message) {
-        return false;
-    }
+	@Override
+	public boolean postContent(Message message) {
+		return false;
+	}
 
-    @Override
-    public boolean authenticate() {
-        return false;
-    }
+	@Override
+	public boolean authenticate() {
+		return false;
+	}
 
-    @Override
-    public boolean isAuthenticated() {
-        return false;
-    }
+	@Override
+	public boolean isAuthenticated() {
+		return false;
+	}
 
-    public void handleMessage(String messageFromTwilio) {
-        // process XML
-        // make a message
-        // put content in
-        // wrap that in a conversation
-        // save the conversation
-        // wheeeeeeeeeee
-    }
+	public void handleMessage(TwilioSmsRequest request) {
+		Message msg = new Message();
+		msg.setCreated(LocalDateTime.now());
+		msg.setAuthor(request.from);
+		msg.setContent(request.body);
 
-    public void setConfig(ProviderConfig config) {
-        this.config = config;
-    }
+		Conversation conv = new Conversation();
+		conv.getMessages().add(msg);
+		conversationService.saveConversation(conv);
+	}
+
+	public void setConfig(ProviderConfig config) {
+		this.config = config;
+	}
 }
+
--- src/main/java/edu/rit/asksg/web/TwilioController.java
+++ src/main/java/edu/rit/asksg/web/TwilioController.java
@@ -1,6 +1,9 @@
 package edu.rit.asksg.web;
 
+import edu.rit.asksg.dataio.TwilioProvider;
 import edu.rit.asksg.domain.Twilio;
+import edu.rit.asksg.domain.TwilioSmsRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.stereotype.Controller;
@@ -11,8 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/twilios")
 public class TwilioController {
 
-    @RequestMapping(value = "/receive")
-    public ResponseEntity<String> receiveSMS() {
-        return null;
-    }
+	@Autowired
+	TwilioProvider provider;
+
+	@RequestMapping(value = "/receive")
+	public ResponseEntity<String> receiveSMS(TwilioSmsRequest request) {
+		provider.handleMessage(request);
+		//TODO this needs to return a "TwiML" XML document for Twilio
+		return null;
+	}
 }
