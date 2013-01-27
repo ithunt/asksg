4188c87b67a7726ffeb61f46d0481c7fba43b564
dge9788 <dgesciguy@gmail.com>
2013-01-27 16:53:31 -0500

    Working on sending twilio messages. Receiving is still broken.

--- src/main/java/edu/rit/asksg/dataio/TwilioProvider.java
+++ src/main/java/edu/rit/asksg/dataio/TwilioProvider.java
@@ -1,5 +1,9 @@
 package edu.rit.asksg.dataio;
 
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.SmsFactory;
+import com.twilio.sdk.resource.instance.Sms;
 import edu.rit.asksg.domain.Conversation;
 import edu.rit.asksg.domain.Message;
 import edu.rit.asksg.domain.ProviderConfig;
@@ -8,6 +12,7 @@ import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -31,7 +36,22 @@ public class TwilioProvider implements ContentProvider {
 
 	@Override
 	public boolean postContent(Message message) {
-		return false;
+		// this assumes that the config will have our Twilio SID assigned to the username.
+		TwilioRestClient twc = new TwilioRestClient(config.getUsername(), config.getAuthenticationToken());
+		Map<String, String> vars = new HashMap<String, String>();
+		vars.put("Body", message.getContent());
+		//TODO: not sure if we'll always want this phone number
+		vars.put("From", twc.getAccount().getAvailablePhoneNumbers().iterator().next().getPhoneNumber());
+		vars.put("To", message.getAuthor());
+
+		SmsFactory smsFactory = twc.getAccount().getSmsFactory();
+		try {
+			Sms sms = smsFactory.create(vars);
+			
+		} catch (TwilioRestException e) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
--- src/main/java/edu/rit/asksg/web/TwilioController.java
+++ src/main/java/edu/rit/asksg/web/TwilioController.java
@@ -1,20 +1,20 @@
 package edu.rit.asksg.web;
 
 import edu.rit.asksg.dataio.TwilioProvider;
-import edu.rit.asksg.domain.Twilio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Map;
 
-@RooWebJson(jsonObject = Twilio.class)
 @Controller
 @RequestMapping("/twilios")
 public class TwilioController {
@@ -22,22 +22,29 @@ public class TwilioController {
 	@Autowired
 	TwilioProvider provider;
 
-	@RequestMapping(value = "/receive", method = RequestMethod.POST, produces = "text/xml")
-	public
-	@ResponseBody
-	TwiMLResponse receiveSMS(@RequestBody Map<String, String> request) {
+	@RequestMapping(value = "/receive", method = RequestMethod.POST)//, produces = "text/xml")
+	//public ResponseEntity<String> receiveSMS(@RequestBody Map<String, String> request) {
+	public @ResponseBody
+	ResponseEntity<String> receiveSMS(@RequestBody Map<String, String> request) {
 		provider.handleMessage(request);
+		Logger logger = LoggerFactory.getLogger("");
+		logger.error("TwilioController: receiveSMS called");
 
 		//TODO: I'm not sure what this XML document will actually look like to Twilio.
-		TwiMLResponse response = new TwiMLResponse();
+		/*TwiMLResponse response = new TwiMLResponse();
 		response.Sms = "Thank you for contacting ASKSG.";
-		return response;
+		return response;*/
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("content_type", "text/xml");
+
+		return new ResponseEntity<String>("Please work, Twilio :(", headers, HttpStatus.OK);
 	}
 
 	/*
 	 * Used to respond to Twilio.
 	 */
+	/*
 	private class TwiMLResponse {
 		public String Sms;
-	}
+	}*/
 }
