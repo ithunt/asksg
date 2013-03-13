794c10ee6048a886435853408401e82743c4c8c1
ian <ithunt0@gmail.com>
2013-03-12 20:49:33 -0400

    Facebook pull another feed support

--- src/main/java/edu/rit/asksg/domain/Facebook.java
+++ src/main/java/edu/rit/asksg/domain/Facebook.java
@@ -1,6 +1,7 @@
 package edu.rit.asksg.domain;
 
 import edu.rit.asksg.dataio.ContentProvider;
+import edu.rit.asksg.dataio.SubscriptionProvider;
 import edu.rit.asksg.domain.config.SpringSocialConfig;
 import flexjson.JSON;
 import org.joda.time.LocalDateTime;
@@ -14,71 +15,37 @@ import org.springframework.social.facebook.api.Comment;
 import org.springframework.social.facebook.api.Post;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RooJavaBean
 @RooToString
 @RooJpaEntity
 @RooJson
-public class Facebook extends Service implements ContentProvider {
+public class Facebook extends Service implements ContentProvider, SubscriptionProvider {
 
 	private static final transient Logger logger = LoggerFactory.getLogger(Facebook.class);
 
 	@JSON(include = false)
 	public List<Conversation> getNewContent() {
-		// Right now this only will get the posts on the wall of the authenticated user, but
-		// it's easy enough to tell getFeed to get posts from other users as well/instead.
-		List<Conversation> conversations = new ArrayList<Conversation>();
 		final org.springframework.social.facebook.api.Facebook facebookApi = getFacebookApi();
-		List<Post> posts = facebookApi.feedOperations().getFeed();
-		for (Post post : posts) {
-			Conversation conversation = new Conversation();
-			Message message = new Message();
-			message.setConversation(conversation);
-			message.setContent(post.getMessage());
-			message.setAuthor(facebookApi.userOperations().getUserProfile(post.getFrom().getId()).getName());
-			message.setCreated(new LocalDateTime(post.getCreatedTime()));
-			conversation.getMessages().add(message);
-			for (Comment comment : post.getComments()) {
-				Message commentMsg = new Message();
-				commentMsg.setConversation(conversation);
-				commentMsg.setContent(comment.getMessage());
-				commentMsg.setAuthor(facebookApi.userOperations().getUserProfile(comment.getFrom().getId()).getName());
-				commentMsg.setCreated(new LocalDateTime(comment.getCreatedTime()));
-			}
-			conversations.add(conversation);
-		}
-		return conversations;
+		return parseFacebookFeed(facebookApi.feedOperations().getFeed(), facebookApi);
 	}
 
 	@JSON(include = false)
 	public List<Conversation> getContentSince(LocalDateTime datetime) {
-		// Right now this only will get the posts on the wall of the authenticated user, but
-		// it's easy enough to tell getFeed to get posts from other users as well/instead.
-		List<Conversation> conversations = new ArrayList<Conversation>();
 		final org.springframework.social.facebook.api.Facebook facebookApi = getFacebookApi();
-		List<Post> posts = facebookApi.feedOperations().getFeed();
-		for (Post post : posts) {
-			if (!datetime.isBefore(new LocalDateTime(post.getCreatedTime()))) {
-				continue;
-			}
-			Conversation conversation = new Conversation();
-			Message message = new Message();
-			message.setConversation(conversation);
-			message.setContent(post.getMessage());
-			message.setAuthor(facebookApi.userOperations().getUserProfile(post.getFrom().getId()).getName());
-			message.setCreated(new LocalDateTime(post.getCreatedTime()));
-			conversation.getMessages().add(message);
-			for (Comment comment : post.getComments()) {
-				Message commentMsg = new Message();
-				commentMsg.setConversation(conversation);
-				commentMsg.setContent(comment.getMessage());
-				commentMsg.setAuthor(facebookApi.userOperations().getUserProfile(comment.getFrom().getId()).getName());
-				commentMsg.setCreated(new LocalDateTime(comment.getCreatedTime()));
-			}
-			conversations.add(conversation);
-		}
-		return conversations;
+		final List<Post> posts = facebookApi.feedOperations().getFeed();
+
+        //filter out posts by date
+        List<Post> filtered = new ArrayList<Post>();
+        for(Post post : posts) {
+            if(datetime.isAfter(new LocalDateTime(post.getCreatedTime()))) {
+                filtered.add(post);
+            }
+        }
+
+        return parseFacebookFeed(filtered, facebookApi);
 	}
 
 	public boolean postContent(Message message) {
@@ -104,5 +71,37 @@ public class Facebook extends Service implements ContentProvider {
 		//todo make safe
 		return (org.springframework.social.facebook.api.Facebook) ((SpringSocialConfig) this.getConfig()).getApiBinding();
 	}
+
+    @JSON(include = false)
+    public Collection<Conversation> getContentFor(SocialSubscription socialSubscription) {
+        final org.springframework.social.facebook.api.Facebook facebookApi = getFacebookApi();
+        return parseFacebookFeed(facebookApi.feedOperations().getFeed(socialSubscription.getHandle()), facebookApi);
+    }
+
+
+    protected List<Conversation> parseFacebookFeed(List<Post> posts, org.springframework.social.facebook.api.Facebook facebookApi) {
+        final List<Conversation> conversations = new ArrayList<Conversation>();
+        for (Post post : posts) {
+
+            Message message = new Message();
+            Conversation conversation = new Conversation(message);
+            message.setConversation(conversation);
+            message.setContent(post.getMessage());
+            message.setAuthor(facebookApi.userOperations().getUserProfile(post.getFrom().getId()).getName());
+            message.setCreated(new LocalDateTime(post.getCreatedTime()));
+
+            for (Comment comment : post.getComments()) {
+                Message commentMsg = new Message();
+                commentMsg.setConversation(conversation);
+                commentMsg.setContent(comment.getMessage());
+                commentMsg.setAuthor(facebookApi.userOperations().getUserProfile(comment.getFrom().getId()).getName());
+                commentMsg.setCreated(new LocalDateTime(comment.getCreatedTime()));
+                conversation.getMessages().add(commentMsg);
+            }
+            conversations.add(conversation);
+        }
+        return conversations;
+
+    }
 
 }
