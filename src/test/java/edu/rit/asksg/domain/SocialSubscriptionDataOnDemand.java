cc162e0cf2900b7ffbf7033cb01f3ebb76c9894c
ian <ithunt0@gmail.com>
2013-03-12 20:48:38 -0400

    SocialSubscriptions and ProviderConfig of subscriptions. SubscriptionProvider interface

---
+++ src/test/java/edu/rit/asksg/domain/SocialSubscriptionDataOnDemand.java
@@ -0,0 +1,7 @@
+package edu.rit.asksg.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = SocialSubscription.class)
+public class SocialSubscriptionDataOnDemand {
+}
