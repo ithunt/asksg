657b4bc4c5305f5f067950866f9b965efd9addb5
Christopher Wood <christopherwood07@gmail.com>
2013-04-06 18:20:40 -0400

    The line isn't being plotted, but the JSON test data should be formatted good enough

--- src/main/webapp/js/analyticsui.js
+++ src/main/webapp/js/analyticsui.js
@@ -1,16 +1,16 @@
 var margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
+    width = 800 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var parseDate = d3.time.format("%Y%m%d").parse;
+// var parseDate = d3.time.format("%Y%m%d").parse;
 
-var x = d3.time.scale()
+var x = d3.scale.linear()
     .range([0, width]);
 
 var y = d3.scale.linear()
     .range([height, 0]);
 
-var color = d3.scale.category10();
+// var color = d3.scale.category10();
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -20,70 +20,106 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-var line = d3.svg.line()
-    .interpolate("basis")
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.temperature); });
-
 var svg = d3.select(".mainAnalytics").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", function(error, data) {
-  console.log("OPENING THE D3 DATA");
-  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "date"; }));
+d3.json("test.json", function(error, data) {
+  alert("OPENING THE D3 DATA");
+  console.log(data);
 
+  // parse each of the times now??
   data.forEach(function(d) {
-    d.date = parseDate(d.date);
+    console.log(d.values);
+    d.values.forEach(function(t) {
+      // console.log(t.time);
+      // t.time = parseDate(t.time);
+      // console.log(t.time);
+    })
   });
 
-  var cities = color.domain().map(function(name) {
-    return {
-      name: name,
-      values: data.map(function(d) {
-        return {date: d.date, temperature: +d[name]};
-      })
-    };
+  // Pull all of the times into a single collection
+  scores = []
+  times = []
+  data.forEach(function(d) {
+    console.log(d.values);
+    d.values.forEach(function(t) {
+      times = times.concat(t.time);
+      scores = scores.concat({"time":t.time, "value":t.sentiment});
+    })
   });
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
+  // Set the x/y range
+  console.log(times);
+  console.log(scores);
 
-  y.domain([
-    d3.min(cities, function(c) { return d3.min(c.values, function(v) { return v.temperature; }); }),
-    d3.max(cities, function(c) { return d3.max(c.values, function(v) { return v.temperature; }); })
-  ]);
+  // Set the domains...
+  x.domain(d3.extent(times));
+  y.domain(d3.extent([-1,1]));
+
+  console.log(x.domain());
+    // [
+    //   d3.min(data, function(word) { return d3.min(word.values, function(v) { return v.time; }); }),
+    //   d3.max(data, function(word) { return d3.max(word.values, function(v) { return v.time; }); })
+    // ]);
+  //   d3.extent(times)
+  // );
+
+
+  console.log(d3.extent(times));
 
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+      .call(xAxis)
+      .append("text")
+      .style("text-anchor", "end")
+      .text("Time");
 
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
-    .append("text")
+      .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Temperature (ºF)");
+      .text("Sentiment Score");
 
-  var city = svg.selectAll(".city")
-      .data(cities)
-    .enter().append("g")
-      .attr("class", "city");
+  var line = d3.svg.line()
+    .interpolate("basis")
+    .x(function(d) { return x(d.time); })
+    .y(function(d) { return y(d.value); });
 
-  city.append("path")
-      .attr("class", "line")
-      .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return color(d.name); });
 
-  city.append("text")
-      .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-      .attr("x", 3)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
+// TODO: not parsing correctly...
+  svg.selectAll(".line")
+    .data(scores)
+    .enter().append("path")
+    .attr("class", "line")
+    .attr("d", line);
+
+  // svg.append("path")
+  //   .datum(data[0].values)
+  //   .attr("class", "line")
+  //   .attr("d", line);
+
+  // var city = svg.selectAll(".city")
+  //     .data(cities)
+  //     .enter().append("g")
+  //     .attr("class", "city");
+
+  // city.append("path")
+  //     .attr("class", "line")
+  //     .attr("d", function(d) { return line(d.values); })
+  //     .style("stroke", function(d) { return color(d.name); });
+
+  // city.append("text")
+  //     .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+  //     .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
+  //     .attr("x", 3)
+  //     .attr("dy", ".35em")
+  //     .text(function(d) { return d.name; });
 });
