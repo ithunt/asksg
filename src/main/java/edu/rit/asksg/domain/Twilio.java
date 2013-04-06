102c1c8468d966f2a4790ef3edd64d42553fbb66
dge9788 <dgesciguy@gmail.com>
2013-04-06 13:15:33 -0400

    Fix for Twilio sending. Gets the To number from the original message's author. Issue #30.

--- src/main/java/edu/rit/asksg/domain/Twilio.java
+++ src/main/java/edu/rit/asksg/domain/Twilio.java
@@ -54,7 +54,9 @@ public class Twilio extends Service implements ContentProvider {
 		Map<String, String> vars = new HashMap<String, String>();
 		vars.put("Body", message.getContent());
 		vars.put("From", config.getPhoneNumber());
-		vars.put("To", message.getAuthor());
+		// This is the fix recommended by Ryan because the UI can't be relied on to send a 'to' >_>
+		String number = ((Message[])message.getConversation().getMessages().toArray())[0].getAuthor();
+		vars.put("To", number);
 
 		SmsFactory smsFactory = twc.getAccount().getSmsFactory();
 		try {
@@ -90,6 +92,7 @@ public class Twilio extends Service implements ContentProvider {
 		msg.setConversation(conv);
 
 		conv.setService(this);
+		conv.setExternalId(smsSid);
 
 		conversationService.saveConversation(conv);
 	}
