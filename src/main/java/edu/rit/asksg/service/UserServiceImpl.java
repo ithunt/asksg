5b6d74cd9baa9073982c4fe9afd8347341da5e72
ian <ithunt0@gmail.com>
2013-04-25 17:43:11 -0400

    /users/current to get details on the currently logged in user

--- src/main/java/edu/rit/asksg/service/UserServiceImpl.java
+++ src/main/java/edu/rit/asksg/service/UserServiceImpl.java
@@ -4,7 +4,6 @@ package edu.rit.asksg.service;
 import edu.rit.asksg.domain.AsksgUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
